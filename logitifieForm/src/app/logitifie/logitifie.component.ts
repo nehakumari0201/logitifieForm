d1c6d2e8d0cbba0fa73d755545ff549569987061
neha <neha.kumari732@gmail.com>
2022-04-29 16:56:14 +0530

    working

--- logitifieForm/src/app/logitifie/logitifie.component.ts
+++ logitifieForm/src/app/logitifie/logitifie.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,17 +13,38 @@ export class LogitifieComponent implements OnInit {
   public showPassword: boolean = true;
   public hide: boolean = true;
   selection = 'Foo';
-
-  constructor() { }
+  form!:FormGroup;
+  constructor( private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form=this.fb.group({
+      companyName: ['',Validators.required],
+        officeAddress: [''],
+        fname: [''],
+        lname: [''],
+        phoneNo: [''],
+        altphoneNo: [''],
+        email: [''],
+        userName: [''],
+        password: ['',[Validators.required ,Validators.pattern("^(?=.*[A-Z])(?=.*[0-8])(?=.*[a-z]).{8,}$")]],
+        confirm_password: [''],
+        hearAbout: [''],
+        traficType: [''],
+        paymentType: [''],
+        file:['']
+      });
   }
   selectFile(event :any) {
     this.selectedFiles = event.target.files;
     console.log(this.selectedFiles);
+    this.form.patchValue({file: this.selectedFiles[0].name});
 }
 
 public togglePasswordVisibility(): void {
   this.showPassword = !this.showPassword;
 }
+saveDetails(form: FormGroup) {
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(form.value, null));
+  console.log(form.value)}
+
 }
--- logitifieForm/src/app/records.service.ts
+++ logitifieForm/src/app/records.service.ts
@@ -4,6 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecordsService {
-
+records:IRecords[]=[];
   constructor() { }
+  getAll():IRecords[]{
+    return this.records;
+  }
+  save(data:any):void{
+      this.records.push(data);
+      console.log(data);
+    }
+
+
+}
+
+export interface IRecords{
+  companyName:string;
+  officeAddress:string;
+  fname:string;
+  lname:string;
+  phoneNo:number;
+  altphoneNo:number;
+  email:string;
+  userName:string;
+  password:string;
+  confirm_password:string;
+  hearAbout:string;
+  traficType:string;
+  paymentType:string;
+  file:File;
 }
