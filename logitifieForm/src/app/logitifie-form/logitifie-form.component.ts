74494af34b5d8d216e9e6a78ebfa3afbc38c2661
neha <neha.kumari732@gmail.com>
2022-04-29 17:14:06 +0530

    working

--- logitifieForm/src/app/logitifie-form/logitifie-form.component.ts
+++ logitifieForm/src/app/logitifie-form/logitifie-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-logitifie-form',
@@ -11,17 +12,41 @@ export class LogitifieFormComponent implements OnInit {
   selectedFiles: any;
   checked = false;
   disabled = false;
-
-  constructor() { }
+  form!:FormGroup;
+  constructor( private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.form=this.fb.group({
+      companyName: ['',Validators.required],
+        officeAddress: [''],
+        fname: [''],
+        lname: [''],
+        phoneNo: [''],
+        altphoneNo: [''],
+        email: [''],
+        userName: [''],
+        password: ['',[Validators.required ,Validators.pattern("^(?=.*[A-Z])(?=.*[0-8])(?=.*[a-z]).{8,}$")]],
+        confirm_password: [''],
+        hearAbout: [''],
+        traficType: [''],
+        paymentType: [''],
+        file:['']
+      });
+
+
   }
+
   selectFile(event :any) {
     this.selectedFiles = event.target.files;
     console.log(this.selectedFiles);
+    this.form.patchValue({file: this.selectedFiles[0].name});
 }
 
 public togglePasswordVisibility(): void {
   this.showPassword = !this.showPassword;
 }
+saveDetails(form: FormGroup) {
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(form.value, null));
+  console.log(form.value)}
+
 }
